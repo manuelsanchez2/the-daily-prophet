a276f360f4ae19a9dd5b35da67a45de727100077
Manuel Sanchez <manuel.sanchez@zeit.de>
2024-10-03 12:03:37 +0200

    fix: fix layout and brightness of logo

--- src/lib/components/App.svelte
+++ src/lib/components/App.svelte
@@ -12,7 +12,7 @@
         <GridDebug active={false} />
     
         <figure class="z-30">
-            <img src={dailyProphet} alt="The Daily Prophet Logo" />
+            <img class="brightness-0" src={dailyProphet} alt="The Daily Prophet Logo" />
         </figure>
     
         <div class="px-z-ds-12">
@@ -29,9 +29,9 @@
                                 Sirius Black’s escape from Azkaban sent shockwaves throughout the wizarding world. Up until that moment, the infamous wizard had been thought of as one of the most dangerous and malevolent criminals ever to be locked away in the soul-crushing prison. For twelve years, Black had languished in his cell, presumed guilty of one of the greatest betrayals in recent magical history—the betrayal of James and Lily Potter, which led to their deaths at the hands of Lord Voldemort. To make matters worse, it was believed that in his desperate attempt to escape justice, Sirius had murdered twelve innocent Muggles in broad daylight, leaving only one finger of Peter Pettigrew, his supposed final victim, as evidence of the massacre. When news of his escape broke, the wizarding world was plunged into fear and panic. Sirius Black was not just a convict; he was a man tied to the darkest chapter of recent magical history. He had been the Potter family's trusted friend, even named godfather to their son, Harry. But in a shocking twist, he was revealed as the one who had handed over James and Lily to Voldemort, condemning them to death and leaving Harry an orphan. The mere idea that such a close confidant of the Potters could have betrayed them so callously intensified the fear surrounding him.
                             </p>
             
-                            <div class="lg:-mt-3 flex gap-z-ds-12 lg:flex-col">
-                                <span class="font-letter text-[200px] leading-none">H</span>
-                                <p class="lg:-mt-10">
+                            <div class="mt-z-ds-16 flex flex-col sm:flex-row lg:flex-col gap-z-ds-12">
+                                <span class="font-letter text-[120px] lg:text-[200px] leading-none">H</span>
+                                <p class="-mt-4 sm:mt-0 lg:-mt-10">
                                     The Ministry of Magic issued warnings far and wide, and Azkaban's strict security measures, meant to be impenetrable, had somehow failed to contain him. Sirius Black was now at large, and there was only one possible target: Harry Potter, the Boy Who Lived. Many believed that Black was driven by a twisted loyalty to the Dark Lord. Voldemort may have fallen, but his followers, including those who had been imprisoned, never forgot their mission. Rumors swirled that Black’s escape was part of a plan to seek out Harry, finish what Voldemort had started, and kill the boy who had miraculously survived that fatal night.
                                 </p>
                             </div>
@@ -39,7 +39,7 @@
     
                     </div>
               
-                    <div class="lg:col-span-3 lg:pl-z-ds-4 my-z-ds-16 md:-mt-6 lg:my-0">
+                    <div class="lg:col-span-3 lg:pl-z-ds-4 my-z-ds-16 lg:my-0">
                         <!-- svelte-ignore a11y_media_has_caption -->
                         <video class="w-full" src={siriusVideo} autoplay loop controls playsinline></video>
 
