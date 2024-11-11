2c565880b690191200dbb3c8f3c84344de713301
Manuel Sanchez <manuel.sanchez@zeit.de>
2024-11-11 09:16:20 +0100

    feat: add analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script>
 	import BreakpointsDebug from '$lib/components/BreakpointsDebug.svelte';
 	import '../app.css';
+	import { inject } from '@vercel/analytics'
+
+	inject()
 </script>
 
 <slot />
 <BreakpointsDebug />
+
